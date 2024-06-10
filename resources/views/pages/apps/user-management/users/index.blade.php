3a2e160203a475d98f932e4775c8c08e05851222
mezyanlahcen88 <mezyan.lahcen88@gmail.com>
2024-06-11 00:11:52 +0100

    updates

--- resources/views/pages/apps/user-management/users/index.blade.php
+++ resources/views/pages/apps/user-management/users/index.blade.php
@@ -21,7 +21,7 @@
                 </div>
                 <!--end::Search-->
                 <!--begin::Export buttons-->
-                <div id="kt_datatable_example_1_export" class="d-none"></div>
+                <div id="kt_datatable_example_export" class="d-none"></div>
                 <!--end::Export buttons-->
             </div>
             <div class="d-flex">
@@ -108,7 +108,7 @@
                             <th class="w-10px pe-2">
                                 <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
                                     <input class="form-check-input" type="checkbox" data-kt-check="true"
-                                        data-kt-check-target="#kt_datatable_example_1 .form-check-input"
+                                        data-kt-check-target="#kt_datatable_example .form-check-input"
                                         value="1" />
                                 </div>
                             </th>
